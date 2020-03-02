6b7373f7de3fe8de2c3f5f5d8ff3e11f5af8b219
Charles Ferreira <charles.dev@live.com>
2020-03-02 09:59:24 -0300

    Finaliza o custom video player

--- custom-video-player/script.js
+++ custom-video-player/script.js
@@ -29,10 +29,24 @@ function stopVideo() {
 }
 
 // Update progress & timestamp
-function updateProgress() {}
+function updateProgress() {
+  progress.value = (video.currentTime / video.duration) * 100;
+
+  // get minutes
+  let mins = Math.floor(video.currentTime / 60);
+  if (mins < 10) mins = '0' + String(mins);
+
+  // get seconds
+  let secs = Math.floor(video.currentTime % 60);
+  if (secs < 10) secs = '0' + String(secs);
+
+  timestamp.innerHTML = `${mins}:${secs}`;
+}
 
 // Set video time to progress
-function setVideoProgress() {}
+function setVideoProgress() {
+  video.currentTime = (+progress.value * video.duration) / 100;
+}
 
 // Event listeners
 video.addEventListener('click', toggleVideoStatus);
