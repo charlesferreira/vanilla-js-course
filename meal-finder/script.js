9b53a4a476a6c407b2df89ae72271a132ecc510e
Charles Ferreira <charles.dev@live.com>
2020-03-04 14:33:51 -0300

    Adiciona single meal

--- meal-finder/script.js
+++ meal-finder/script.js
@@ -47,5 +47,56 @@ function searchMeal(e) {
   }
 }
 
+// Fetch meal by ID
+function getMealById(mealId) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
+    .then(res => res.json())
+    .then(data => {
+      const meal = data.meals[0];
+
+      addMealtToDOM(meal);
+    });
+}
+
+// Add meal to DOM
+function addMealtToDOM(meal) {
+  const ingredients = [];
+
+  for (let i = 1; i <= 20; i++) {
+    if (meal[`strIngredient${i}`]) {
+      ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`);
+    } else {
+      break;
+    }
+  }
+
+  singleMealElement.innerHTML = `
+  <div class="single-meal">
+    <h1>${meal.strMeal}</h1>
+    <img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
+    <div class="single-meal-info">
+      ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ''}
+      ${meal.strArea ? `<p>${meal.strArea}</p>` : ''}
+    </div>
+    <div class="main">
+      <p>${meal.strInstructions}</p>
+      <h2>Ingredients</h2>
+      <ul>
+        ${ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')}
+      </ul>
+    </div>
+  </div>
+  `;
+}
+
 // Event listeners
 submit.addEventListener('submit', searchMeal);
+
+mealsElement.addEventListener('click', e => {
+  const mealInfo = e.path.find(item => item.classList && item.classList.contains('meal-info'));
+
+  if (mealInfo) {
+    const mealId = mealInfo.getAttribute('data-mealId');
+    getMealById(mealId);
+  }
+});
